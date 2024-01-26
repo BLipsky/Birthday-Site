f62479127ec84aa2091678739a1118a3b3f7344b
Ben_Lipsky <Benlipsky@icloud.com>
2024-01-26 14:59:31 -0500

    New

---
+++ script.js
@@ -0,0 +1,8 @@
+function displayOne(){-
+
+    document.getElementById('text1').innerText = '' 
+    document.getElementById('text2').innerText = ''
+    document.getElementById('img1') = 
+    document.getElementById('img2') =
+    
+}

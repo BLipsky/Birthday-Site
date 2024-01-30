fa8bb3f8930237852ff36978fdc9de3f70990a77
Ben_Lipsky <Benlipsky@icloud.com>
2024-01-30 13:48:56 -0500

    almost

--- script.js
+++ script.js
@@ -14,17 +14,17 @@ function displayTwo(){
 }
 function displayThree(){
 
-    document.getElementById('text1').innerText = ""
-    document.getElementById('text2').innerText = ""
-    document.getElementById('img1').src = '.jpg'
-    document.getElementById('img2').src = '.jpg'
+    document.getElementById('text1').innerText = "On February 23rd, 1868, W.E.B. Du Bois, a prominent African American civil rights activist, sociologist, and co-founder of the National Association for the Advancement of Colored People (NAACP), was born in Great Barrington, Massachusetts. Du Bois became a leading intellectual figure in the civil rights movement, advocating for racial equality, education, and political representation for African Americans. His influential works, including 'The Souls of Black Folk,' contributed significantly to the discourse on race and social justice in the United States. W.E.B. Du Bois's legacy continues to have a profound impact on the fight for civil rights and equality."
+    document.getElementById('text2').innerText = "W.E.B. Du Bois dedicated his life to challenging racial discrimination and promoting social justice. A trailblazer in academia, he became the first African American to earn a doctorate from Harvard University in 1895. Du Bois's scholarly work delved into issues of race, class, and the intersectionality of social inequalities. He emphasized the importance of higher education for African Americans and worked tirelessly to break down racial barriers. As a co-founder of the NAACP in 1909, Du Bois played a crucial role in advocating for civil rights, leading campaigns against lynching, segregation, and disenfranchisement. Throughout his lifetime, Du Bois remained a tireless advocate for equality and justice, leaving an indelible mark on the ongoing struggle for civil rights in the United States."
+    document.getElementById('img1').src = 'Dubois1.jpg'
+    document.getElementById('img2').src = 'Dubois2.jpg'
 }
 function displayFour(){
 
-    document.getElementById('text1').innerText = ""
-    document.getElementById('text2').innerText = ""
-    document.getElementById('img1').src = '.jpg'
-    document.getElementById('img2').src = '.jpg'
+    document.getElementById('text1').innerText = "On February 23rd, 1945, during World War II, the iconic photograph of the U.S. flag being raised on Mount Suribachi on the island of Iwo Jima was taken. This historic moment was a significant event in the Battle of Iwo Jima, a fierce and pivotal conflict between the United States Marine Corps and the Imperial Japanese Army. The capture of Mount Suribachi was a key objective for the U.S. forces, as it provided a strategic vantage point for observing and coordinating the ongoing battle. The raising of the American flag symbolized a significant achievement and boosted morale among the U.S. troops. The photograph, captured by Associated Press photographer Joe Rosenthal, went on to become one of the most iconic images of the war and won the Pulitzer Prize for Photography. The Battle of Iwo Jima continued for several more weeks, with intense fighting and heavy casualties on both sides."
+    document.getElementById('text2').innerText = "The Battle of Iwo Jima, which began on February 19, 1945, was a critical engagement in the Pacific theater of World War II. The island's strategic importance lay in its airfields, which could serve as a base for U.S. bombers targeting the Japanese mainland. The intense fighting on Iwo Jima resulted in some of the bloodiest and fiercest battles of the entire war. The U.S. Marines faced determined Japanese defenders who were deeply entrenched in a network of tunnels and fortified positions. The struggle for Iwo Jima lasted for over a month, and despite capturing Mount Suribachi relatively early in the campaign, the overall battle proved to be exceptionally challenging for both sides. The iconic flag-raising moment on February 23rd became a symbol of American determination and sacrifice in the face of formidable opposition, and the Battle of Iwo Jima stands as a testament to the high cost of victory in the Pacific theater."
+    document.getElementById('img1').src = 'Flag1.webp'
+    document.getElementById('img2').src = 'Flag2.jpg'
 }
 function displayFive(){
 

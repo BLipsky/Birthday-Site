1c9b4d875c56a1b28889211303f220e5e3e63a49
Ben_Lipsky <Benlipsky@icloud.com>
2024-01-31 13:50:48 -0500

    Finalish

--- script.js
+++ script.js
@@ -40,3 +40,23 @@ function displaySix(){
     document.getElementById('img1').src = 'Russia1.svg'
     document.getElementById('img2').src = 'Russia2.jpg'
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    var modal = document.getElementById("myModal");
+    var btn = document.getElementById("openModalBtn");
+    var span = document.getElementsByClassName("close")[0];
+
+    btn.onclick = function () {
+        modal.style.display = "block";
+    }
+
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function (event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    }
+});
